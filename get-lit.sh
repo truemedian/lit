e28c48e9c60f4d9a33ced8b5501ac14719a03016
Nameless <truemedian@gmail.com>
2024-12-02 09:40:53 -0600

    work: cleanup

--- get-lit.sh
+++ get-lit.sh
@@ -11,12 +11,40 @@ LUVI_VERSION=${LUVI_VERSION:-2.15.0}
 LIT_VERSION=${LIT_VERSION:-3.8.5}
 LUVIT_VERSION=${LUVIT_VERSION:-latest}
 
+_lit_zip="${LUVI_PREFIX}/lit.zip"
+_luvit_zip="${LUVI_PREFIX}/luvit.zip"
+_luvi_bin="${LUVI_PREFIX}/luvi"
+_lit_bin="${LUVI_PREFIX}/lit"
+_luvit_bin="${LUVI_PREFIX}/luvit"
+
+_newline="
+"
+
+_cleanup=""
+cleanup() {
+    _exit=$1
+
+    if [ -n "${_cleanup}" ]; then
+        echo "[*] Cleaning up"
+        echo "${_cleanup}" | xargs rm -f
+    fi
+    exit "${_exit}"
+}
+
+add_cleanup() {
+    [ -n "${_cleanup}" ] && _cleanup="${_cleanup}${_newline}"
+    _cleanup="${_cleanup}$1"
+}
+
+trap 'echo "[#] Cancelling installation"; cleanup 1' INT TERM
+
 # download a file from $1 and save it as $2
 download() {
     _url=$1
     _file=$2
 
     echo "[*] Downloading ${_file} from ${_url}"
+    add_cleanup "${_file}"
 
     # --retry requires curl 7.12.3, from 2004
     _status="$(curl --retry 5 --retry-delay 5 -#Lfo "${_file}" -w "%{http_code}" "${_url}")"
@@ -42,9 +70,9 @@ version_gte() {
 }
 
 # allow selecting latest, but real versions need a v prefix
-[ "${LUVI_VERSION}" != "latest" ]  && LUVI_VERSION="v${LUVI_VERSION}"
-[ "${LIT_VERSION}" != "latest" ]  && LIT_VERSION="v${LIT_VERSION}"
-[ "${LUVIT_VERSION}" != "latest" ]  && LUVIT_VERSION="v${LUVIT_VERSION}"
+[ "${LUVI_VERSION}" != "latest" ] && LUVI_VERSION="v${LUVI_VERSION}"
+[ "${LIT_VERSION}" != "latest" ] && LIT_VERSION="v${LIT_VERSION}"
+[ "${LUVIT_VERSION}" != "latest" ] && LUVIT_VERSION="v${LUVIT_VERSION}"
 
 _luvi_url="https://github.com/luvit/luvi/releases/download/${LUVI_VERSION}/luvi-regular-${LUVI_OS}_${LUVI_ARCH}"
 _lit_url="https://lit.luvit.io/packages/luvit/lit/${LIT_VERSION}.zip"
@@ -56,46 +84,40 @@ fi
 
 echo "[+] Installing luvit, lit and luvi to ${LUVI_PREFIX}"
 
-_lit_zip="${LUVI_PREFIX}/lit.zip"
-_luvit_zip="${LUVI_PREFIX}/luvit.zip"
-_luvi_bin="${LUVI_PREFIX}/luvi"
-_lit_bin="${LUVI_PREFIX}/lit"
-_luvit_bin="${LUVI_PREFIX}/luvit"
-
 # Download Luvi, and the sources for Lit and Luvit
 
-download "${_luvi_url}" "${_luvi_bin}" || exit 1
-download "${_lit_url}" "${_lit_zip}" || exit 1
-download "${_luvit_url}" "${_luvit_zip}" || exit 1
+download "${_luvi_url}" "${_luvi_bin}" || cleanup 1
+download "${_lit_url}" "${_lit_zip}" || cleanup 1
+download "${_luvit_url}" "${_luvit_zip}" || cleanup 1
 
 # Install luvi
 
 chmod +x "${_luvi_bin}"
 if [ ! -x "${_luvi_bin}" ]; then
     echo "[!] Could not make luvi executable"
-    exit 1
+    cleanup 1
 fi
 
 # Install lit
 
 echo "[*] Creating lit from lit.zip"
+add_cleanup "${_lit_bin}"
 "${_luvi_bin}" "${_lit_zip}" -- make "${_lit_zip}" "${_lit_bin}" "${_luvi_bin}"
 if [ ! -x "${_lit_bin}" ]; then
     echo "[!] Could not create lit"
-    exit 1
+    cleanup 1
 fi
 
 # Install luvit
 
 echo "[*] Creating luvit from luvit.zip"
+add_cleanup "${_luvit_bin}"
 "${_lit_bin}" make "${_luvit_zip}" "${_luvit_bin}" "${_luvi_bin}"
 if [ ! -x "${_luvit_bin}" ]; then
     echo "[!] Could not create luvit"
-    exit 1
+    cleanup 1
 fi
 
-# Cleanup lit.zip and luvit.zip
-
-rm -f "${_lit_zip}" "${_luvit_zip}"
-
 echo "[+] Installation complete at ${LUVI_PREFIX}"
+
+cleanup 0
